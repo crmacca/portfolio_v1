3bb59a78c1c7fe32aec3138b5626216cbe467709
Christian <55185704+crmacca@users.noreply.github.com>
2023-10-23 21:48:04 +1100

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,4 +1,20 @@
 const data = {
-    projects: []
+    projects: [
+        {
+            name: 'ChrisMC Developments',
+            href: 'https://discord.gg/VzHDV8bDwG',
+            icon: 'https://media.discordapp.net/attachments/989834645812154410/989834717627047996/discordBrighter.png?ex=6544c6ea&is=653251ea&hm=fd0e5608cb7854ee27b378ee20bf38510df68e51fe229a5d1823f858bebf3caf&=&width=910&height=910'
+        },
+        {
+            name: 'FoodMate V3 (ROBLOX)',
+            href: 'https://devforum.roblox.com/t/foodmate-v3-free-cafe-ordering-system/1926043',
+            icon: 'https://media.discordapp.net/attachments/989834645812154410/1165964036131782767/DALLE_2023-10-23_21.44.29_-_Vector_illustration_of_a_modern_logo_with_the_name_FoodMate._The_logo_is_on_a_white_background._It_features_the_word_FoodMate_in_a_sleek_rounded_.png?ex=6548c3a0&is=65364ea0&hm=b05c06df06b17d6411ce7d2c379273f8a7efc496a3c6a94c02e70f4077d7d218&=&width=910&height=910'
+        },
+        {
+            name: 'Social (Discord Bot)',
+            href: 'https://github.com/crmacca/NotificationsBot',
+            icon: 'https://media.discordapp.net/attachments/989834645812154410/1165964817908105236/SocialLogo.png?ex=6548c45b&is=65364f5b&hm=c763f05f4140fd508df1262361b69da56aba56d840030f43028e3bd3b601f0d6&=&width=910&height=910'
+        }
+    ]
 }
 export default data
